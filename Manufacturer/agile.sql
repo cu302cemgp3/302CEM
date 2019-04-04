cfa33e9a4ebde4d60a2a6f0a0fe2fb78aa2b2958
Toby Chan <laplap1998@gmail.com>
2019-04-04 19:10:27 +0800

    System beta 1.0
    Add to delivery table when the shipping is arranged

---
+++ Manufacturer/agile.sql
@@ -0,0 +1,222 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2019 年 04 月 04 日 13:07
+-- 伺服器版本: 10.1.34-MariaDB
+-- PHP 版本： 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `agile`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `delivery`
+--
+
+CREATE TABLE `delivery` (
+  `delivery_id` int(11) NOT NULL,
+  `item_id` varchar(11) NOT NULL,
+  `retailer_id` varchar(11) NOT NULL,
+  `order_id` varchar(11) NOT NULL,
+  `timeslot` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `delivery`
+--
+
+INSERT INTO `delivery` (`delivery_id`, `item_id`, `retailer_id`, `order_id`, `timeslot`) VALUES
+(11, 'I001', 'R001', '1', '2019-04-04 11:00:00'),
+(12, 'I001', 'R001', '1', '2019-04-03 09:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `inv`
+--
+
+CREATE TABLE `inv` (
+  `product_id` varchar(11) NOT NULL,
+  `product_description` text NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `inv`
+--
+
+INSERT INTO `inv` (`product_id`, `product_description`, `amount`) VALUES
+('I001', 'Juice', 98700),
+('I002', 'Cable G48', 99550),
+('I003', 'Charger M320', 23469),
+('I004', 'Pen', 42200),
+('I005', 'Mouse', 50000);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `man`
+--
+
+CREATE TABLE `man` (
+  `exp_key` varchar(11) NOT NULL,
+  `item_no` varchar(11) DEFAULT NULL,
+  `expected_shipment_date` text,
+  `qty` int(11) DEFAULT NULL,
+  `price_per_item` int(11) DEFAULT NULL,
+  `manufacturer_id` text,
+  `retailer_id` text,
+  `handle` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `man`
+--
+
+INSERT INTO `man` (`exp_key`, `item_no`, `expected_shipment_date`, `qty`, `price_per_item`, `manufacturer_id`, `retailer_id`, `handle`) VALUES
+('1', 'I001', '2019-02-20 12:30:00', 100, 20, 'M001', 'R001', 1),
+('2', 'I002', '2019-02-20 16:25:00', 50, 61, 'M001', 'R001', 0),
+('3', 'I003', '2019-04-03 15:10:00', 10, 40, 'M001', 'R001', 0),
+('6', 'I002', '2019-03-20 16:25:00', -3, -20, 'M001', 'R001', 0),
+('7', 'I001', '2019-04-03 10:00:00', 10, 1, 'M001', 'R002', 0),
+('8', 'I003', '2019-04-03 08:00:00', 10, 1, 'M001', 'R001', 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `retailer`
+--
+
+CREATE TABLE `retailer` (
+  `retailer_id` varchar(11) NOT NULL,
+  `location` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `retailer`
+--
+
+INSERT INTO `retailer` (`retailer_id`, `location`) VALUES
+('R001', 'Hong Kong Island'),
+('R003', 'Tsuen Wan');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `time`
+--
+
+CREATE TABLE `time` (
+  `time_id` int(11) NOT NULL,
+  `timeslot` datetime NOT NULL,
+  `available` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `time`
+--
+
+INSERT INTO `time` (`time_id`, `timeslot`, `available`) VALUES
+(2, '2019-04-03 09:00:00', 0),
+(3, '2019-04-03 10:00:00', 1),
+(4, '2019-04-03 14:00:00', 1),
+(5, '2019-04-03 16:00:00', 1),
+(6, '2019-04-04 09:00:00', 1),
+(7, '2019-04-04 14:00:00', 1),
+(8, '2019-04-04 16:00:00', 1),
+(9, '2019-04-05 09:00:00', 1),
+(10, '2019-04-03 11:00:00', 1),
+(11, '2019-04-05 14:00:00', 1),
+(12, '2019-04-03 16:00:00', 1);
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`delivery_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `retailer_id` (`retailer_id`);
+
+--
+-- 資料表索引 `inv`
+--
+ALTER TABLE `inv`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- 資料表索引 `man`
+--
+ALTER TABLE `man`
+  ADD PRIMARY KEY (`exp_key`),
+  ADD KEY `item_no` (`item_no`);
+
+--
+-- 資料表索引 `retailer`
+--
+ALTER TABLE `retailer`
+  ADD PRIMARY KEY (`retailer_id`);
+
+--
+-- 資料表索引 `time`
+--
+ALTER TABLE `time`
+  ADD PRIMARY KEY (`time_id`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `delivery`
+--
+ALTER TABLE `delivery`
+  MODIFY `delivery_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- 使用資料表 AUTO_INCREMENT `time`
+--
+ALTER TABLE `time`
+  MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- 已匯出資料表的限制(Constraint)
+--
+
+--
+-- 資料表的 Constraints `delivery`
+--
+ALTER TABLE `delivery`
+  ADD CONSTRAINT `delivery_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `inv` (`product_id`),
+  ADD CONSTRAINT `delivery_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `man` (`exp_key`),
+  ADD CONSTRAINT `delivery_ibfk_3` FOREIGN KEY (`retailer_id`) REFERENCES `retailer` (`retailer_id`);
+
+--
+-- 資料表的 Constraints `man`
+--
+ALTER TABLE `man`
+  ADD CONSTRAINT `man_ibfk_1` FOREIGN KEY (`item_no`) REFERENCES `inv` (`product_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- Manufacturer/system.py
+++ Manufacturer/system.py
@@ -60,62 +60,67 @@ def arrange():
     qorder = order_cursor.fetchall()
     for q in qorder:
         print(q)
-    order = input("Order ID: ")
-    cursor2 = db.cursor()
-    if cursor2.execute("select * from man where exp_key = %s", order):
-        # query = cursor2.fetchone()
-        # cursor2.close()
-        amount = input("How many? ")
-        cursor_minus = db.cursor()
-        cursor_minus.execute("select man.exp_key, man.item_no, inv.amount from inv join man on inv.product_id = man.item_no where exp_key = %s", order)
-        inventory = cursor_minus.fetchone()
-        reduction = int(inventory[2]) - int(amount)
-        cursor_update1 = db.cursor()
-        cursor_update1.execute("UPDATE inv SET amount = %s WHERE product_id = %s", (reduction, inventory[1]))
-        db.commit()
-        cursor_update2 = db.cursor()
-        cursor_update2.execute("update man set handle = 1 where exp_key = %s", inventory[0])
-        db.commit()
-        print("Which time slot? ")
-        cursor4 = db.cursor()
-        cursor4.execute("select * from time where available = 1")
-        query = cursor4.fetchall()
-        for q in query:
-            print(q[0], q[1].strftime("%m/%d/%Y, %H:%M:%S"), q[2])
-        time = input("Time slot ID: ")
 
-        cursor5 = db.cursor()
-        cursor5.execute("UPDATE time SET available = 0 WHERE time_id = %s", time)
-        db.commit()
-        cursor6 = db.cursor()
-        cursor6.execute("select timeslot from time where time_id = %s", time)
-        query2 = cursor6.fetchone()
-
-        print("You are going to deliver " + amount + " piece of " + order + " on " + str(query2[0]))
-        ex = input("Add to delivery queue? y/N? ")
-        cursor3 = db.cursor()
-        cursor3.execute(
-            "SELECT man.item_no, man.retailer_id, man.exp_key, time.timeslot FROM man JOIN time ON man.exp_key = %s WHERE time.time_id = %s", (order, time))
-        query3 = cursor3.fetchone()
-        if ex == 'y':
-            cursor_indeliv = db.cursor()
-            print(query3[0])
-            print(query3[1])
-            print(query3[2])
-            print(query3[3])
-            print(query3)
-            # var = (query3[0], query3[1], query3[2], query3[3])
-            # sql = cursor_indeliv.execute("INSERT INTO `delivery`(`item_id`, `retailer_id`, `order_id`, `timeslot`) VALUES ('%s','%s','%s','%s')", var)
-            # print(sql)
-            date = str(datetime.datetime.now().date())
-            filename = date+"output.csv"
-            with open(filename, 'a', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                for row in query3:
-                    writer.writerow(str(row))
-
+    if qorder == None:
+        print("There are no any order left.")
+        main()
     else:
-        print("no such item")
+        order = input("Order ID: ")
+        cursor2 = db.cursor()
+        if cursor2.execute("select * from man where exp_key = %s", order):
+            amount = input("How many? ")
+            cursor_minus = db.cursor()
+            cursor_minus.execute("select man.exp_key, man.item_no, inv.amount from inv join man on inv.product_id = man.item_no where exp_key = %s", order)
+            inventory = cursor_minus.fetchone()
+            reduction = int(inventory[2]) - int(amount)
+
+            cursor_update1 = db.cursor()
+            cursor_update1.execute("UPDATE inv SET amount = %s WHERE product_id = %s", (reduction, inventory[1]))
+            db.commit()
+
+            cursor_update2 = db.cursor()
+            cursor_update2.execute("update man set handle = 1 where exp_key = %s", inventory[0])
+            db.commit()
+
+            print("Which time slot? ")
+            cursor4 = db.cursor()
+            cursor4.execute("select * from time where available = 1")
+            query = cursor4.fetchall()
+            for q in query:
+                print(q[0], q[1].strftime("%m/%d/%Y, %H:%M:%S"), q[2])
+            time = input("Time slot ID: ")
+
+            updatetime = db.cursor()
+            updatetime.execute("UPDATE time SET available = 0 WHERE time_id = %s", time)
+            db.commit()
+
+            selecttime = db.cursor()
+            selecttime.execute("select timeslot from time where time_id = %s", time)
+            query2 = selecttime.fetchone()
+
+            print("You are going to deliver " + amount + " piece of " + order + " on " + str(query2[0]))
+            ex = input("Add to delivery queue? y/N? ")
+            cursor3 = db.cursor()
+            cursor3.execute(
+                "SELECT man.item_no, man.retailer_id, man.exp_key, time.timeslot FROM man JOIN time ON man.exp_key = %s WHERE time.time_id = %s", (order, time))
+            query3 = cursor3.fetchone()
+            if ex == 'y':
+                cursor_indeliv = db.cursor()
+                newtime = query3[3].strftime("%Y-%m-%d %H:%M:%S")
+                sql = "insert into delivery (item_id, retailer_id, order_id, timeslot) VALUES (%s,%s,%s,%s)"
+                var = (query3[0], query3[1], query3[2], newtime)
+                cursor_indeliv.execute(sql, var)
+                db.commit()
+                # print(sql)
+
+                date = str(datetime.datetime.now().date())
+                filename = date+"output.csv"
+                with open(filename, 'a', newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([query3[0], query3[1], query3[2], query3[3].strftime("%m/%d/%Y, %H:%M:%S")])
+
+        else:
+            print("no such item")
 
 def vieworder():
     print("-----------------------------------\n"
